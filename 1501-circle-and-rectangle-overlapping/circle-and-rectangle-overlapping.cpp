127ad5e76bc841b972a8d7924514ac4ed0882ede
adithya1258 <adithyavardhan2005@gmail.com>
2026-09-19 12:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (68.85%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter,
+                      int x1, int y1, int x2, int y2) {
+
+        int r = radius;
+        int x = xCenter;
+        int y = yCenter;
+
+        int x3 = x1;
+        int y3 = y2;
+
+        int x4 = x2;
+        int y4 = y1;
+
+        // Distance squared from center to each corner
+        int dc1 = (x - x1) * (x - x1) + (y - y1) * (y - y1);
+        int dc2 = (x - x2) * (x - x2) + (y - y2) * (y - y2);
+        int dc3 = (x - x3) * (x - x3) + (y - y3) * (y - y3);
+        int dc4 = (x - x4) * (x - x4) + (y - y4) * (y - y4);
+
+        // Check corners
+        if (dc1 <= r * r || dc2 <= r * r ||
+            dc3 <= r * r || dc4 <= r * r) {
+            return true;
+        }
+
+        // Center is vertically within rectangle
+        if (y >= y1 && y <= y2) {
+            int d1 = abs(x - x1);
+            int d2 = abs(x - x2);
+
+            if (d1 <= r || d2 <= r) {
+                return true;
+            }
+        }
+
+        // Center is horizontally within rectangle
+        if (x >= x1 && x <= x2) {
+            int d1 = abs(y - y1);
+            int d2 = abs(y - y2);
+
+            if (d1 <= r || d2 <= r) {
+                return true;
+            }
+        }
+
+       
+        if (x >= x1 && x <= x2 &&
+            y >= y1 && y <= y2) {
+            return true;
+        }
+
+        return false;
+    }
+};
